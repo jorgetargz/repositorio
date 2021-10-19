b6100056d22f110e633d4497b03e78297b7ca987
jorgetargz <jorgemartin99@outlook.com>
2021-10-19 10:34:43 +0200

    pi0pki

--- Main/Main/src/main/java/main/Main.java
+++ Main/Main/src/main/java/main/Main.java
@@ -1,7 +1,33 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Menu");
+            System.out.println("Opcion 1");
+            System.out.println("Opcion 2");
+            System.out.println("Opcion 3");
+            System.out.println("Opcion 4");
+            System.out.println("0. Salir");
+            System.out.print("Introduce un numero: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        } while (opcion != 0);
     }
 }
